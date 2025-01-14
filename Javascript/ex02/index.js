b58efabb5aeaa300b1c96a929b151c69fe36d643
Henrique Galinha <hgalinha1999@hotmail.com>
2025-01-14 12:07:40 +0000

    Start of javascript

---
+++ Javascript/ex02/index.js
@@ -0,0 +1,4 @@
+let post = 'teste de teste';
+let math = 200 - post.length;
+
+console.log('You have written', post.length, 'caracters, you still have left', math, 'of 200 caracters.');
---
+++ Javascript/teste.js
@@ -0,0 +1,8 @@
+var message = 'Hello';
+var names = 'Henrique';
+
+var len = names.lenght;
+
+console.log(message, 'there,', names + '!');
+
+console.log(names.length);
