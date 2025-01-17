c13c00214ff078318a16c28386c512aa438c0f6e
Henrique Galinha <hgalinha1999@hotmail.com>
2025-01-17 07:48:02 +0000

    update

---
+++ Javascript/exercices/ex11/DOM Challenge Starting Files/index.js
@@ -0,0 +1,11 @@
+let teste = document.querySelectorAll('li')[0];
+
+teste.style.color = 'red';
+teste.style.backgroundColor = 'yellow'
+teste.style.fontSize = '2em'
+teste.style.borderRadius = '20px'
+
+btn = document.querySelector('button').style.backgroundColor = 'red'
+
+document.querySelector('h1').classList.add('new');
+document.querySelector('.new').innerHTML = 'Test';
