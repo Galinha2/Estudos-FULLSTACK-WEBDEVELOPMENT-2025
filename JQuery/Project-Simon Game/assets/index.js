f13d11a923fb4b51f0d47882f133005758c969d1
Henrique Galinha <hgalinha1999@hotmail.com>
2025-01-24 18:31:10 +0000

    UPDATE SIMON GAME

--- JQuery/Project-Simon Game/assets/index.js
+++ JQuery/Project-Simon Game/assets/index.js
@@ -1,12 +1,40 @@
 function simonPick() {
-    let rand = Math.ceil(Math.random() * 4);
 
-    if (rand === 1) {
-        
-    };
+    //COMEÇO DO JOGO 
+    $(document).on('keydown', function(event) {
+
+        //COMEÇO DO JOGO COM A TECLA SPACE
+        if (event.code === 'Space'){
+
+            while (true) {
+                //ESCOLHE A COR SELECIONADA DE 1 A 4 POR ORDEM DELAS
+                let rand = Math.ceil(Math.random() * 4);
+
+                //ATRIBUI OS NUMEROS ÀS CORES E PLAY NO SOM
+                if (rand === 1) {
+                    $('.green').css('background-color', 'rgb(0, 255, 0)');
+                    $('#green')[0].play();
+                };
+                if (rand === 2) {
+                    $('.red').css('background-color', 'red');
+                    $('#red')[0].play();
+                };
+                if (rand === 3) {
+                    $('.yellow').css('background-color', 'yellow');
+                    $('#yellow')[0].play();
+                };
+                if (rand === 4) {
+                    $('.blue').css('background-color', 'blue');
+                    $('#blue')[0].play();
+                };
+            }
+        };
+    })
     $('.square').on('click', function(event) {
         console.log(event);
     });
+
 };
 
-simonPick();
+simonPick(); 
+
