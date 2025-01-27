64170388ba4eb94523c7ec2004751028f30b2703
Henrique Galinha <hgalinha1999@hotmail.com>
2025-01-27 18:21:40 +0000

    update

--- JQuery/Project-Simon Game/assets/index.js
+++ JQuery/Project-Simon Game/assets/index.js
@@ -25,6 +25,7 @@ function simonPick() {
         };
     }
 
+    //FAZ COM QUE OS BOTÕES VOLTEM AO NORMAL
     function defaultBtns() {
         $('.green').css('background-color', 'rgb(0, 128, 0,0.801)');
 
@@ -35,21 +36,39 @@ function simonPick() {
         $('.blue').css('background-color', 'rgba(0, 0, 111, 0.801)');
     }
 
+    //ONDE  O JOGO CORRE
     function jogoCompleto() {
         var armazenaRand = [];
         let armazenaUser = [];
 
+        //NO INTEREVALO METE OS BOTÕES DEFAULT
         setInterval(function() {
             defaultBtns();
             
+            //NA PAUSA EM 1 EM 1 SEGUNDO CORRE ISTO
             setTimeout(function() {
                 let rand = Math.ceil(Math.random() * 4);
-                colorPicker(rand);
+                
+                //MOSTRA A COR RELACIONADA COM O NUM
                 let color = colorPicker(rand);
                 armazenaRand.push(color);
                 console.log(armazenaRand);
+                
+                //MOSTRA OS BOTÕES QUE O USER CARREGOU
+                //PRECISO METER PAUSA PRA USER INPUT AQUI EM BAIXO
+                let user = checkAnswer();
+                armazenaUser.push(user);
+                console.log(armazenaUser);
+
             },500); 
         }, 1000);
+    }
+
+    function checkAnswer() {
+        $('.square').on('click', function(event) {
+            let clicado = event.target.classList[1];
+            return clicado;
+        });
     }
 
     //COMEÇO DO JOGO 
@@ -60,16 +79,8 @@ function simonPick() {
             
             jogoCompleto()
         }
-            for (let c = 0; c < 21; c++) {
-                
-
-        };
     })
-    $('.square').on('click', function(event) {
-        console.log(event);
-    });
-
-    
+  
 };
 
 simonPick(); 
