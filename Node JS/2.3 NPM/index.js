0d94182e718e473f089bb600d320ca85928d83d6
Henrique Galinha <hgalinha1999@hotmail.com>
2025-02-17 17:47:16 +0000

    aulas

---
+++ Node JS/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from 'sillyName';
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
+
+import superheroes from 'superheros';
+const name = superheroes.random();
+
+console.log(`My super hero name is ${name}`);
---
+++ Node JS/teste.js
@@ -0,0 +1,7 @@
+module.exports = {
+    nome: 'Henrique',
+    apelido: 'Galinha',
+    idade: 26
+};
+
+console.log(module.exports);
