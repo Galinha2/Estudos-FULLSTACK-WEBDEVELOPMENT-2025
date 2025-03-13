48e939d0f47e957e26b6e29d0ae6a452d414c534
Henrique Galinha <hgalinha1999@hotmail.com>
2025-03-13 15:10:39 +0000

    aula env

--- 12-Authentication/9.3+Sessions+and+Cookies/index.js
+++ 12-Authentication/9.3+Sessions+and+Cookies/index.js
@@ -26,6 +26,9 @@ app.use(
     secret: 'TOPSECRET',
     resave: false,
     saveUninitialize: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
   })
 );
 
@@ -77,11 +80,15 @@ app.post("/register", async (req, res) => {
           console.error("Error hashing password:", err);
         } else {
           console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await  db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          const user = result.rows[0];
+          req.login(user, (err => {
+            console.log(err)
+            res.redirect("/secrets");
+          }))
         }
       });
     }
