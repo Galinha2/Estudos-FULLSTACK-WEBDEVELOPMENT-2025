0a14fe3b7b74086932d949e5562cd35af39bd97c
Henrique Galinha <hgalinha1999@hotmail.com>
2025-03-17 16:22:12 +0000

    REact

--- 13-REACT/jsx-babel/src/App.jsx
+++ 13-REACT/jsx-babel/src/App.jsx
@@ -1,10 +1,23 @@
+const Card = ({title}) => {
+  return (
+    <h3 className="movies">{title}</h3>
+  )
+};
+
+const name = 'Henrique'
+const lname = 'Galinha'
+
 const App = () => {
   return (
     <>
-      <h1>Hello World!</h1>
-      <a href="#">Teste</a>
+      <h1>Hello {name + ' ' + lname}!</h1>
+      <ul>
+        <li><a href="#"><Card title='Avatar'/></a></li>
+        <li><a href="#"><Card title='Star Wars'/></a></li>
+        <li><a href="#"><Card title='Minecraft'/></a></li>
+      </ul>
     </>
   )
-}
+};
 
 export default App
