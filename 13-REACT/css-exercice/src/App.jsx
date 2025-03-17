489665c0db5d1654c1e1fda3c1effe31128a37eb
Henrique Galinha <hgalinha1999@hotmail.com>
2025-03-17 16:35:27 +0000

    react

---
+++ 13-REACT/css-exercice/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <img className="img" src="https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027.jpg" alt="" />
+      <img className="img" src="https://s2-g1.glbimg.com/c1tS_axTjV_qDkmMeMs3wYZCgGY=/0x0:5472x3648/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/H/v/pTatikTlSIWRuTzd0JwA/j9a6180.jpg" alt="" />
+      <img className="img" src="https://wallpapers.com/images/featured/imagens-muito-legais-40it5k0y58kfe71d.jpg" alt="" />
+    </>
+  )
+}
+
+export default App
